0ff7fb32b44d7115c9376153023ff3c7268f0998
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 13:06:32 +0200

    Add scroll listener to Navbar
    This commit adds a scroll listener to the Navbar component.
    
    The listener is used to add a class to the Navbar component when the user
    scrolls down the page.
    
    The class is used to change the Navbar's background color.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,21 @@ const Navbar = ({}) => {
   const [toggle, setToggle] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <nav
       className={`${
