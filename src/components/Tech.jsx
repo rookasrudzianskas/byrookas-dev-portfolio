782ad1fb039f635d2524ab6debc365beb7017308
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 15:38:26 +0200

    Animation -01

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = ({}) => {
   return (
-    <div>
-
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology, index) => (
+        <div key={index} className="w-28 h-28">
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Tech;
+export default SectionWrapper(Tech, "");
 // by Rokas with ❤️
