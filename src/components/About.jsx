39fcd92aa719399114530e1e6bdf345fc2d5e593
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 14:43:05 +0200

    HOC

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,30 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+
+    </motion.div>
   </Tilt>
 )
 
@@ -40,5 +64,5 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
 // by Rokas with ❤️
