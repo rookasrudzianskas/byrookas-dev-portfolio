1739eeb39a7e0eb37b46d0fff2fb31b60cc068f5
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 15:42:16 +0200

    animation

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -17,7 +17,10 @@ const Ball = (props) => {
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={ [0, 0, 0.05] } />
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh
+        castShadow
+        receiveShadow
+        scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color='#fff8eb'
