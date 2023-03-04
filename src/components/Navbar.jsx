31b3e8e226970285ebbca4fd1ca3da3ee752a393
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 13:00:10 +0200

    Add logo to navbar
    - Add logo to navbar
    - Add link to home page
    - Add link to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,20 @@ const Navbar = ({}) => {
       }`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} className="w-9 h-9 object-contain" alt="logo"/>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Rokas Rudzianskas &nbsp;
+            <span className='sm:block hidden'> | byRookas</span>
+          </p>
+        </Link>
       </div>
 
     </nav>
