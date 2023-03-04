f2bb2fb9a6c8afcf8190151ab94cbd221b645afd
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 13:04:20 +0200

    feat: add navbar
    - add navbar
    - add navbar links
    - add navbar toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,8 +33,51 @@ const Navbar = ({}) => {
             <span className='sm:block hidden'> | byRookas</span>
           </p>
         </Link>
-      </div>
 
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((nav) => {
+            return (
+              <li
+                key={nav.id}
+                className={`${
+                  active === nav.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
