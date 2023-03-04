6cc3c1cf53c03dfd38931a6881fc0cf69bdc1ddc
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 15:41:13 +0200

    Add decal to ball
    The decal was missing from the ball.

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -26,10 +26,9 @@ const Ball = (props) => {
           flatShading
         />
         <Decal
+          map={decal}
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
           flatShading
         />
       </mesh>
