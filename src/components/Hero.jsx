2936446d8b847e074768dd511a259dee45fc7e6f
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 14:07:42 +0200

    animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,16 @@ const Hero = ({}) => {
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Rokas</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I make 3D visuals, user interfaces, <br className='sm:block hidden' />
+            and web apps come to life!
+          </p>
+        </div>
       </div>
     </section>
   );
